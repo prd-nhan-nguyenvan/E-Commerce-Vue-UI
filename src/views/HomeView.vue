a4660d34c0a9c643dda798f4e1c0b64fde993b65
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-10-01 17:20:42 +0700

    feat(user-product-page): display product grid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,9 @@
 <template>
-  <div class="container">
-    <h1 class="mb-4">Products</h1>
-    <div v-for="product in products" :key="product.id" class="card mb-4">
-      <!-- Product Image -->
-      <img :src="product.image" class="card-img-top" alt="Product Image" />
-
-      <!-- Product Information -->
-      <div class="card-body">
-        <h5 class="card-title">{{ product.name }}</h5>
-        <p class="card-text">Secondary text</p>
-      </div>
-
-      <!-- Actions -->
-      <div class="card-footer d-flex justify-content-between">
-        <button class="btn btn-primary">Action 1</button>
-        <button class="btn btn-secondary">Action 2</button>
-      </div>
-    </div>
+  <div class="container my-5">
+    <ProductListView></ProductListView>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { getAllProducts } from '@/services/product.service'
-
-const products = ref([])
-
-const fetchProducts = async () => {
-  try {
-    const productData = await getAllProducts()
-    products.value = productData
-  } catch (error) {
-    console.error('Error fetching products:', error)
-  }
-}
-
-onMounted(fetchProducts) // Fetch products when the component is mounted
-
-// Function to handle adding to cart (you can build this later)
-const addToCart = (product) => {
-  console.log('Adding product to cart:', product)
-  // Add cart logic here
-}
+import ProductListView from './products/ProductListView.vue'
 </script>
-
-<style scoped></style>
---
+++ src/views/products/ProductListView.vue
@@ -0,0 +1,94 @@
+<template>
+  <!-- Product Card Grid View -->
+  <div class="row">
+    <div class="col-md-4 mb-4" v-for="product in products" :key="product.id">
+      <div class="card h-100 shadow-sm border-light">
+        <img
+          v-if="product.image"
+          :src="product.image"
+          alt="Product Image"
+          class="card-img-top img-thumbnail"
+          style="height: 200px; object-fit: cover"
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ product.name }}</h5>
+          <p class="card-text">{{ product.description }}</p>
+          <p class="card-text">
+            <strong>Price: </strong>
+            <span v-if="product.on_sell">
+              <span class="badge bg-danger ms-2">On Sale!</span>
+
+              <span class="text-danger fw-bold">
+                {{ formatCurrency(product.sell_price) }}
+              </span>
+              <br />
+              <span class="small text-muted text-decoration-line-through">
+                {{ formatCurrency(product.price) }}
+              </span>
+            </span>
+            <span v-else>
+              <span class="fw-bold">{{ formatCurrency(product.price) }}</span>
+            </span>
+          </p>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary w-100">Add to Cart</button>
+          <router-link
+            :to="{ name: 'ProductDetail', params: { slug: product.slug } }"
+            class="btn btn-link w-100"
+          >
+            View Detail</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { useProductStore } from '@/stores/product'
+import { useCategoryStore } from '@/stores/category'
+import { computed, onMounted, ref } from 'vue'
+import Swal from 'sweetalert2'
+
+const productStore = useProductStore()
+const categoryStore = useCategoryStore()
+
+const { fetchProducts } = productStore
+const { fetchCategories } = categoryStore
+
+const recordsOptions = [5, 10, 20, 50]
+
+const currentPage = ref(1)
+const productsPerPage = ref(recordsOptions[1])
+
+const products = computed(() => productStore.products)
+const loading = computed(() => productStore.loading)
+const previous = computed(() => productStore.previous)
+const next = computed(() => productStore.next)
+const error = computed(() => productStore.error)
+const totalPages = computed(() => Math.ceil(productStore.count / productsPerPage.value))
+
+// Methods
+
+const formatCurrency = (price: string) => {
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+    parseFloat(price)
+  )
+}
+
+onMounted(() => {
+  console.log('Loading Products...')
+  fetchProducts()
+
+  console.log('Loading Categories...')
+  fetchCategories()
+})
+</script>
+
+<style lang="css" scoped>
+.card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease-in-out;
+}
+</style>
