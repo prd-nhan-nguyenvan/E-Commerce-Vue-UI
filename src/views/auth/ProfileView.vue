309028d2dd3288a1c1c22e3663e76b999a5ec86b
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-10-08 10:49:55 +0700

    refactor(profile): beautiful edit inline, using icon instead 'edit' text

--- src/views/auth/ProfileView.vue
+++ src/views/auth/ProfileView.vue
@@ -1,96 +1,101 @@
 <template>
   <div class="container my-5">
     <div class="row justify-content-center">
-      <div class="row">
-        <div class="col-md-4 text-center">
-          <!-- Profile Picture -->
-          <img
-            v-if="profile?.profile_picture"
-            :src="profile?.profile_picture"
-            alt="Profile Picture"
-            class="rounded-circle mb-3"
-            width="150"
-            height="150"
-          />
-          <img
-            v-else
-            src="https://isobarscience-1bfd8.kxcdn.com/wp-content/uploads/2020/09/default-profile-picture1.jpg"
-            alt="Default Profile Picture"
-            class="rounded-circle mb-3"
-            width="150"
-            height="150"
-          />
-          <h3>{{ profile?.first_name }} {{ profile?.last_name }}</h3>
-          <p class="text-muted">{{ profile?.role }}</p>
-        </div>
-
-        <div class="col-md-8">
-          <!-- User Info -->
-          <h4 class="mt-4">User Information</h4>
-
-          <!-- Phone Number -->
-          <div class="mb-2">
-            <strong>Phone:</strong>
-            <span v-if="!isEditing.phone">{{
-              profile?.phone_number || 'No phone number available'
-            }}</span>
-            <input
-              v-else
-              v-model="editProfile.phone_number"
-              type="text"
-              class="form-control"
-              placeholder="Enter phone number"
+      <div class="col-lg-4">
+        <div class="card mb-4 text-center">
+          <div class="card-body">
+            <img
+              v-if="profile?.profile_picture"
+              :src="profile?.profile_picture"
+              alt="Profile Picture"
+              class="rounded-circle mb-3"
+              width="150"
+              height="150"
             />
-            <button v-if="!isEditing.phone" class="btn btn-link" @click="startEditing('phone')">
-              Edit
-            </button>
-          </div>
-
-          <!-- Address -->
-          <div class="mb-2">
-            <strong>Address:</strong>
-            <span v-if="!isEditing.address">{{ profile?.address || 'No address available' }}</span>
-            <input
+            <img
               v-else
-              v-model="editProfile.address"
-              type="text"
-              class="form-control"
-              placeholder="Enter address"
+              src="https://isobarscience-1bfd8.kxcdn.com/wp-content/uploads/2020/09/default-profile-picture1.jpg"
+              alt="Default Profile Picture"
+              class="rounded-circle mb-3"
+              width="150"
+              height="150"
             />
-            <button v-if="!isEditing.address" class="btn btn-link" @click="startEditing('address')">
-              Edit
-            </button>
-          </div>
-
-          <!-- Bio -->
-          <div class="mb-2">
-            <strong>Bio:</strong>
-            <p v-if="!isEditing.bio">{{ profile?.bio || 'No bio available' }}</p>
-            <textarea
-              v-else
-              v-model="editProfile.bio"
-              class="form-control"
-              rows="3"
-              placeholder="Enter bio"
-            ></textarea>
-            <button v-if="!isEditing.bio" class="btn btn-link" @click="startEditing('bio')">
-              Edit
-            </button>
+            <h5>{{ profile?.first_name }} {{ profile?.last_name }}</h5>
           </div>
         </div>
       </div>
+      <div class="col-lg-8">
+        <div class="card mb-4">
+          <div class="card-body">
+            <h4 class="">User Information</h4>
+            <hr />
+            <div class="mb-2 d-flex align-items-center">
+              <strong>Phone: &nbsp;</strong>
+              <span v-if="!isEditing.phone">{{
+                profile?.phone_number || 'No phone number available'
+              }}</span>
+              <input
+                v-else
+                v-model="editProfile.phone_number"
+                type="text"
+                class="form-control"
+                placeholder="Enter phone number"
+                style="flex: 1"
+                @keydown.enter="saveChanges"
+                @keydown.esc="cancelEdit"
+              />
+              <button v-if="!isEditing.phone" class="btn btn-link" @click="startEditing('phone')">
+                <i class="material-icons">edit</i>
+              </button>
+            </div>
+            <hr />
+            <div class="mb-2 d-flex align-items-center">
+              <strong>Address:&nbsp;</strong>
+              <span v-if="!isEditing.address">{{
+                profile?.address || 'No address available'
+              }}</span>
+              <input
+                v-else
+                v-model="editProfile.address"
+                type="text"
+                class="form-control"
+                placeholder="Enter address"
+                style="flex: 1"
+                @keydown.enter="saveChanges"
+                @keydown.esc="cancelEdit"
+              />
+              <button
+                v-if="!isEditing.address"
+                class="btn btn-link"
+                @click="startEditing('address')"
+              >
+                <i class="material-icons">edit</i>
+              </button>
+            </div>
+            <hr />
+            <div class="mb-2">
+              <strong>Bio:</strong>
+              <button v-if="!isEditing.bio" class="btn btn-link" @click="startEditing('bio')">
+                <i class="material-icons">edit</i>
+              </button>
+              <p v-if="!isEditing.bio">{{ profile?.bio || 'No bio available' }}</p>
+              <textarea
+                v-else
+                v-model="editProfile.bio"
+                class="form-control"
+                rows="3"
+                placeholder="Enter bio"
+              ></textarea>
+            </div>
+          </div>
+        </div>
 
-      <!-- Save Changes Button -->
-      <div v-if="isEditing.phone || isEditing.address || isEditing.bio" class="text-center mt-3">
-        <button class="btn btn-success" @click="saveChanges">Save Changes</button>
-        <button class="btn btn-secondary" @click="cancelEdit">Cancel</button>
+        <!-- Save Changes Button -->
+        <div v-if="isEditing.phone || isEditing.address || isEditing.bio" class="text-center mt-3">
+          <button class="btn btn-success me-2" @click="saveChanges">Save Changes</button>
+          <button class="btn btn-secondary" @click="cancelEdit">Cancel</button>
+        </div>
       </div>
-
-      <!-- Edit Profile Button -->
-      <div
-        class="text-center mt-4"
-        v-if="!isEditing.phone && !isEditing.address && !isEditing.bio"
-      ></div>
     </div>
   </div>
 </template>
