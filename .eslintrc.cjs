75526d9ba89f471f4bf0a18c9786768828b68946
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-10-22 11:05:28 +0700

    chore: configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,21 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:@typescript-eslint/recommended' // Add this line
   ],
   env: {
     node: true
   },
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser' // Add this line to use the TypeScript parser
   },
+  plugins: [
+    '@typescript-eslint' // Add this line to include the plugin
+  ],
   rules: {
-    'vue/no-mutating-props': 'error'
+    'vue/no-mutating-props': 'error',
+    '@typescript-eslint/no-unused-vars': 'error' // You can configure more TypeScript-specific rules here
   }
 }
